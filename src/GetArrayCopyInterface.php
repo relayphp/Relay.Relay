224109cba602ebc6572d51a292317e3d0d6140e4
Paul M. Jones <pmjones88@gmail.com>
2015-06-07 16:37:14 -0500

    add phpdoc

--- src/GetArrayCopyInterface.php
+++ src/GetArrayCopyInterface.php
@@ -1,7 +1,30 @@
 <?php
+/**
+ *
+ * This file is part of Pipeline for PHP.
+ *
+ * @license http://opensource.org/licenses/MIT MIT
+ *
+ * @copyright 2015, Paul M. Jones
+ *
+ */
 namespace Pipeline\Pipeline;
 
+/**
+ *
+ * Allows queue-building objects to return an array copy.
+ *
+ * @package Pipeline.Pipeline
+ *
+ */
 interface GetArrayCopyInterface
 {
+    /**
+     *
+     * Returns an array copy of the queue.
+     *
+     * @return array
+     *
+     */
     public function getArrayCopy();
 }
--- src/PipelineBuilder.php
+++ src/PipelineBuilder.php
@@ -1,21 +1,67 @@
 <?php
+/**
+ *
+ * This file is part of Pipeline for PHP.
+ *
+ * @license http://opensource.org/licenses/MIT MIT
+ *
+ * @copyright 2015, Paul M. Jones
+ *
+ */
 namespace Pipeline\Pipeline;
 
 use ArrayObject;
 use InvalidArgumentException;
 
+/**
+ *
+ * Builds a Pipeline object.
+ *
+ * @package Pipeline.Pipeline
+ *
+ */
 class PipelineBuilder
 {
+    /**
+     *
+     * Constructor.
+     *
+     * @param callable $resolver A resolver to convert queue entries to
+     * callables.
+     *
+     * @return self
+     *
+     */
     public function __construct(callable $resolver = null)
     {
         $this->resolver = $resolver;
     }
 
+    /**
+     *
+     * Returns a new Pipline instance.
+     *
+     * @param array|ArrayObject|GetArrayCopyInterface $queue The queue for the
+     * Pipeline.
+     *
+     * @return Pipeline
+     *
+     */
     public function newInstance($queue)
     {
         return new Pipeline($this->getArray($queue), $this->resolver);
     }
 
+    /**
+     *
+     * Converts a queue specification to an array.
+     *
+     * @param array|ArrayObject|GetArrayCopyInterface $queue The queue
+     * specification.
+     *
+     * @return array
+     *
+     */
     protected function getArray($queue)
     {
         if (is_array($queue)) {
