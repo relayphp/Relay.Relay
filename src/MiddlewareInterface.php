a82f20a78c9d07f214abeae2b56edc9e9b948b6c
Paul M. Jones <pmjones88@gmail.com>
2015-12-17 14:37:17 -0600

    merge conflicted docblock

--- src/MiddlewareInterface.php
+++ src/MiddlewareInterface.php
@@ -17,9 +17,9 @@ use Psr\Http\Message\ResponseInterface;
  *
  * This interface defines the middleware interface signature required by Relay.
  *
- * Implementing this is completely voluntary, it's mostly useful for indicating that
- * your class is middleware, and to ensure you type-hint the `__invoke()` method
- * signature correctly.
+ * Implementing this is completely voluntary, it's mostly useful for indicating
+ * that your class is middleware, and to ensure you type-hint the `__invoke()`
+ * method signature correctly.
  *
  * @package Relay.Relay
  *
--- src/ResolverInterface.php
+++ src/ResolverInterface.php
@@ -12,17 +12,15 @@ namespace Relay;
 
 /**
  *
-<<<<<<< HEAD
  * Formally defines the resolver interface signature required by Relay.
+ *
+ * This interface defines the interface for a resolver; that is, a callable
+ * capable of converting entries from the middleware queue into a callable
+ * or an implementation of MiddlewareInterface.
  *
  * Implementing this is completely voluntary. It is useful for indicating that
  * a class is intended for use as a resolver, and to ensure the `__invoke()`
  * method is typehinted correctly.
-=======
- * This interface defines the interface for a resolver; that is, a callable
- * capable of converting entries from the middleware queue into a callable
- * or an implementation of MiddlewareInterface.
->>>>>>> 1.x
  *
  * @package Relay.Relay
  *
@@ -30,14 +28,6 @@ namespace Relay;
 interface ResolverInterface
 {
     /**
-<<<<<<< HEAD
-     *
-     * Converts a queue entry to a middleware callable.
-     *
-     * @param mixed $entry The queue entry.
-     *
-     * @return callable|MiddlewareInterface A middleware callable.
-=======
      *
      * Converts a middleware queue entry to a callable or an implementation of
      * MiddlewareInterface.
@@ -45,7 +35,6 @@ interface ResolverInterface
      * @param mixed $entry The middleware queue entry.
      *
      * @return callable|MiddlewareInterface
->>>>>>> 1.x
      *
      */
     public function __invoke($entry);
