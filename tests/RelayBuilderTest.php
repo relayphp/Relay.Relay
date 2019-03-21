f32f10d6188a3dc94a65ac181b40057d82f35ab7
jake johns <jake@jakejohns.net>
2019-03-20 20:27:46 -0400

    Add Builder
    Close #33

---
+++ tests/RelayBuilderTest.php
@@ -0,0 +1,47 @@
+<?php
+namespace Relay;
+
+use ArrayObject;
+use InvalidArgumentException;
+use Traversable;
+
+class RelayBuilderTest extends \PHPUnit\Framework\TestCase
+{
+    protected $relayBuilder;
+
+    protected function setUp()
+    {
+        $this->relayBuilder = new RelayBuilder();
+    }
+
+    public function testArray()
+    {
+        $queue = [];
+        $relay = $this->relayBuilder->newInstance($queue);
+        $this->assertInstanceOf('Relay\Relay', $relay);
+    }
+
+    public function testArrayObject()
+    {
+        $queue = new ArrayObject([]);
+        $relay = $this->relayBuilder->newInstance($queue);
+        $this->assertInstanceOf('Relay\Relay', $relay);
+
+        $relay = $this->relayBuilder->newInstance($queue);
+        $this->assertInstanceOf('Relay\Relay', $relay);
+    }
+
+    public function testTraversable()
+    {
+        $queue = $this->createMock(Traversable::class);
+
+        $relay = $this->relayBuilder->newInstance($queue);
+        $this->assertInstanceOf('Relay\Relay', $relay);
+    }
+
+    public function testInvalidArgument()
+    {
+        $this->expectException('TypeError');
+        $this->relayBuilder->newInstance('bad argument');
+    }
+}
